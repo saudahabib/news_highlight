33894de8271700cedb245a3f35791cb5d754e0d7
saudahabib <saudababs00@gmail.com>
2019-04-14 15:43:47 +0300

    add base URL to config file and load the config class DevConfig on the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
-
+from .config import DevConfig
 # Initializing the application
 app = Flask(__name__)
 
-from app import views 
+#Setting up configurations
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+from app import views
--- app/views.py
+++ app/views.py
@@ -7,5 +7,15 @@ def index():
     '''
     View root page that returns the index page and its data
     '''
-    return render_template('index.html')
-    
+    title = "All the spice, under one roof"
+    message = "Hello World"
+    tags = "Tag, You're it!"
+
+    return render_template('index.html', message = message, tags = tags, title= title)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    '''
+    View news page function that returns page with news
+    '''
+    return render_template('news.html', id = news_id)
