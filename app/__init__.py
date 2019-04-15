ade69cdd52d4cb06118a7db10dadc4778ae45dcf
saudahabib <saudababs00@gmail.com>
2019-04-15 17:40:11 +0300

    change the configuration settings on init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
-# Initializing the application
-app = Flask(__name__, instance_relative_config = True)
 
-#Setting up configurations
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+bootstrap = Bootstrap()
 
+def create_app(config_name):
 
-# Initializing flask extensions
-bootstrap = Bootstrap(app)
+    app = Flask(__name__)
 
-from app import views
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
