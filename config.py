32a3f5a31600e48d16aefc9e57d34038a198b187
saudahabib <saudababs00@gmail.com>
2019-04-15 17:37:27 +0300

    store configuration names from the config.py file in a dictionary

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
@@ -25,3 +28,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
