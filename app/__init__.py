1f0c58bdebff4bb833108ed0da064fdf4ae6a948
saudahabib <saudababs00@gmail.com>
2019-04-15 06:17:19 +0300

    add bootstrap styling to page, inherit templates from bootstrap's base.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 # Initializing the application
 app = Flask(__name__, instance_relative_config = True)
 
 #Setting up configurations
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+
+# Initializing flask extensions
+bootstrap = Bootstrap(app)
 
 from app import views
